dde07220f25a79459b025121db053701db869ade
RANJAN <141898565+mranja@users.noreply.github.com>
2023-10-27 15:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,39 +41,3 @@ document.getElementById("cart").addEventListener("click", function(){
     Total = String(Total).split(".")
     console.log(`The total amount is ${Total[0]}$ and ${Total[1]}cents`)
 })
-
-
-
-
-//  function count_item(){
-//     const counter = {};
-//     ItemNames.forEach(ele => {
-//         if (counter[ele]) {
-//             counter[ele] += 1;
-//         } else {
-//             counter[ele] = 1;
-//          }
-//     });
-//     const counter1 = {};
-//     ItemPrice.forEach(ele => {
-//         if (counter1[ele]) {
-//             counter1[ele] += 1;
-//         } else {
-//             counter1[ele] = 1;
-//          }
-//     });
-//     let k = Object.keys(counter)
-//     let v = Object.values(counter)
-//     for (let i=0 ; i<k.length ; i++){
-//         console.log(`Item name : ${k[i]} - Quantity: ${v[i]}`)
-//     }
-//     let k1 = Object.keys(counter1)
-//     let v1 = Object.values(counter1)
-//     let total=0
-//     for (let i=0; i<k1.length; i++){
-//         let t= k1[i]*v[i]
-//         total+=t
-//     }
-//     let deci = 1-(Math.ceil(total)-total)
-//     console.log(`The total amount is :${Math.floor(total)}$ ${Math.ceil(deci*100)} Cents`)
-//  }
